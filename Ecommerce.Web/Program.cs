41c223f014a6aad763913fb3c704bfdf1e6bbb15
inacio88 <inacio88@outlook.com>
2024-11-01 15:24:22 -0300

    master: list products

--- Ecommerce.Web/Program.cs
+++ Ecommerce.Web/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<HttpClient, HttpClient>();
 
 var app = builder.Build();
 
